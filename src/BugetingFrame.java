befc3859ca7c434a044786663222b9ac05ef079c
Andras J. Farkas <100239574+Faranef@users.noreply.github.com>
2023-11-26 20:46:33 +0100

    0-0-9

--- src/BugetingFrame.java
+++ src/BugetingFrame.java
@@ -13,11 +13,38 @@ public class BugetingFrame extends JInternalFrame
         CreateFrame();
         GetServices();
         CreateLabels();
+
+        //Create tabs with "Previous Month"  "Two Months Ago"
+        //https://docs.oracle.com/javase/tutorial/displayCode.html?code=https://docs.oracle.com/javase/tutorial/uiswing/examples/components/TabbedPaneDemoProject/src/components/TabbedPaneDemo.java
     }
 
     private void CreateLabels()
     {
+        JPanel panelIncome = new JPanel();
+        panelIncome.setLayout(new GridLayout(0,2));
+        panelIncome.add(new Label("Income:"));
+        panelIncome.add(new Label(""));
+        panelIncome.add(new Label("Electricity sold:"));
+        panelIncome.add(new Label("123.45"));
 
+        JPanel panelLoss = new JPanel();
+        panelLoss.setLayout(new GridLayout(0,2));
+        panelLoss.add(new Label("Loss:"));
+        panelLoss.add(new Label());
+        panelLoss.add(new Label("Running Costs:"));
+        panelLoss.add(new Label("123.45"), BorderLayout.EAST);
+        panelLoss.add(new Label("Fuelrods:"));
+        panelLoss.add(new Label("50000"));
+        
+        //panel.add(new Label(TotalProfit.toString()));
+
+
+
+
+
+        this.add(panelIncome, BorderLayout.NORTH);
+        this.add(new JSeparator(), BorderLayout.CENTER);
+        this.add(panelLoss, BorderLayout.PAGE_END);
     }
 
     private void GetServices()
